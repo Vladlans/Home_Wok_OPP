2417800d3f27bd8a32c95c9cdf835907224fbe79
Vlad <blackmor143@yandex.ru>
2023-04-04 15:36:11 +0300

    hm4-lst

--- Units/Shooter.java
+++ Units/Shooter.java
@@ -6,8 +6,28 @@ public abstract class Shooter extends BaseUnit {
     protected int arrowQty;
 
     public Shooter(ArrayList<BaseUnit> name, int attack, int defence, int[] damage, int hpMax, int speed, int x, int y,
-            int supply, int gangSize, float health, int arrowQty) {
+            int supply, int gangSize, int health, int arrowQty) {
         super(name, attack, defence, damage, hpMax, speed, x, y, supply, gangSize, health);
         this.arrowQty = arrowQty;
+    }
+
+    @Override
+    public void step(ArrayList<BaseUnit> enemyTeam, ArrayList<BaseUnit> homeTeam) {
+        if (this.health > 0 && this.arrowQty > 0) {
+            if (findClosest(enemyTeam) != null) {
+                this.attack(findClosest(enemyTeam));
+            } else
+                return;
+        } else
+            return;
+        this.arrowQty = this.arrowQty - 1;
+        for (BaseUnit baseUnit : homeTeam) {
+            if (baseUnit.getName() == "P") {
+                if (baseUnit.supply != 0) {
+                    this.arrowQty += 1;
+                    baseUnit.supply -= 1;
+                }
+            }
+        }
     }
 }
